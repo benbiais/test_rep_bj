485d50ff4e368f608ea318dc7c85f80c6d5f6cd3
benbiais <benbiais@laposte.net>
2014-02-03 11:59:58 +0100

    corrigé syntaxe fautes ponctuation

--- projets bj Python/prog_bj_python.py
+++ projets bj Python/prog_bj_python.py
@@ -1,8 +1,8 @@
 print ('bonjour')
-print ('toto)
+print ('toto')
 # et je rajoute une fonction que voici
-for x in range (1,11) ;
-print (x)
-print ('je compte de 1 a 10')
+for x in range (1,11) :
+    print (x)
+    print ('je compte de 1 a 10')
 # et je rajoute encore un commentaire
 # et encore un autre
