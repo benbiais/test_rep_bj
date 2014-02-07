c6cfa61469d985d28d9f95f3b25afff9168e98f8
benbiais <benbiais@laposte.net>
2014-02-07 15:40:48 +0100

    evolutions et compléments

--- projets bj Python/lect_sonde.py
+++ projets bj Python/lect_sonde.py
@@ -2,6 +2,15 @@ tfile = open("/sys/bus/w1/devices/28-00000485fa13/w&_slave")
 text = tfile.read()
 tfile.close()
 print (text)
+secondline = text.split("\n")[1]
+print (secondline)
+tdata = secondline.split(" ")[9]
+print (tdata)
+temperature = float(tdata[2:])
+print (temperature)
+temp = temperature / 1000
+print temp
+
 
 
 
